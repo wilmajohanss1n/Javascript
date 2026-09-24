3ccddb0e5112965d39a04be7adebc2ff22ae0c8b
Wilma <wijo2604@student.miun.se>
2026-09-24 16:26:05 +0200

    Skrev in variablar för personens information i uppgift 1

--- uppgift1.js
+++ uppgift1.js
@@ -1,2 +1,9 @@
 /* Lösning till uppgift 1. Av Wilma Johansson, 2026 */
 "use strict"
+
+// Information om personen
+const firstname = "Wilma";
+const lastname = "Johansson";
+const age = "20";
+const isStudent = true;
+
