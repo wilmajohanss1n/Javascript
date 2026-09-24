c19d8d2fe956876dbd51d96d4c381da607700dce
Wilma <wijo2604@student.miun.se>
2026-09-24 17:34:28 +0200

    Lade till loop för att skriva ut talen 1-20 i uppgift 4

--- uppgift4.js
+++ uppgift4.js
@@ -1,2 +1,12 @@
 /* Lösning till uppgift 4. Av Wilma Johansson, 2026 */
 "use strict";
+
+// Skriver ut alla heltal från 1-20
+for (let number = 1; number <=20; number++) {
+    console.log(number);
+     }
+
+
+
+
+   
