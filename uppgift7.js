2b881f8c7f8313bb19ecbfa02bc3715cac3f1da8
Wilma <wijo2604@student.miun.se>
2026-09-24 21:04:16 +0200

    Skrivit en array med tal samt skrivit en funktion och anropat funktionen.

--- uppgift7.js
+++ uppgift7.js
@@ -1,3 +1,21 @@
 /* Lösning till uppgift 7. Av Wilma Johansson, 2026 */
 "use strict";
 
+//En array med tal
+const numbers = [5, 10, 15, 20, 25, 30];
+
+//Funktion som räknar ut summan av alla tal i arrayen
+function calculateSum(numbers) {
+    let sum = 0;
+
+    for (let number of numbers) {
+    }
+    return sum;
+}
+
+//Anropar funktionen
+const result = calculateSum(numbers);
+
+
+
+
