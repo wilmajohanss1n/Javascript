6adddc11550cea90939d42ce504c7504f7d2e9a3
Wilma <wijo2604@student.miun.se>
2026-09-24 21:27:14 +0200

    Skrivit ett objekt med information om boken Harry Potter.

---
+++ uppgift8.js
@@ -0,0 +1,10 @@
+/* Lösning till uppgift 8. Av Wilma Johansson, 2026 */
+"use strict";
+
+//Ett objekt som innehåller information om en bok
+const book = {
+    title: "Harry Potter och de vises sten",
+    author: "J.K Rowling",
+    year: 1997
+};
+
