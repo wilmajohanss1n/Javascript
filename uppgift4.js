64966a029e822a03df71688cc539081fbe657ea0
Wilma <wijo2604@student.miun.se>
2026-09-24 17:42:07 +0200

    Lade till villkor för att skriva ut jämna tal i uppgift 4

--- uppgift4.js
+++ uppgift4.js
@@ -6,7 +6,12 @@ for (let number = 1; number <=20; number++) {
     console.log(number);
      }
 
-
+// Skriver ut de jämna talen från 1-20
+for (let number = 1; number <= 20; number++) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }
+}
 
 
    
