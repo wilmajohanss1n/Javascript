095ecb768c2820a60172f816097047214094944f
Wilma <wijo2604@student.miun.se>
2026-09-24 20:47:27 +0200

    Skrivit ut fuktion samt anropat funktionen med olika värden

--- uppgift6.js
+++ uppgift6.js
@@ -1,3 +1,14 @@
 /* Lösning till uppgift 6. Av Wilma Johansson, 2026 */
 "use strict";
 
+//Funktion som beräknar arean av en rektangel
+function calculteArea(width, height) {
+    const area = width * height;
+    return area;
+}
+
+//Anropa funktionen med olika värden
+const area1 = calculteArea(5, 10);
+const area2 = calculteArea(8, 4);
+const area3 = calculteArea(12, 6);
+
