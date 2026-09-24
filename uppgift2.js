33e033379cd97f76ee69e85f434af045ab30c543
Wilma <wijo2604@student.miun.se>
2026-09-24 17:01:15 +0200

    Lade till  beräkningar för totalpris och moms i uppgift 2.

--- uppgift2.js
+++ uppgift2.js
@@ -4,3 +4,15 @@
 // Pris för produkt och antal produkter
 const price= 100;
 const quantity= 3;
+
+// Totalpris för alla produkter
+const total = price * quantity
+
+// Totalpris inklusive 25% moms
+const totalWithVat = total * 1.25;
+
+// Skriver ut resultat
+console.log("Pris: " + price + " kr");
+console.log("Antal: " + quantity);
+console.log("Total: " + total + " kr");
+console.log("Totalt inklusive moms: " + totalWithVat + " kr");
