2c744978bda15a0dd903554ba84d4902bfd68d52
Wilma <wijo2604@student.miun.se>
2026-09-24 20:25:38 +0200

    Lade till array metoder och avslutade uppgit 5.

--- uppgift5.js
+++ uppgift5.js
@@ -1,3 +1,24 @@
 /* Lösning till uppgift 5. Av Wilma Johansson, 2026 */
 "use strict";
 
+// En array med maträtter
+const dishes = ["Pizza", "Tacos", "Sushi", "Hamburgare", "Pasta"];
+
+//Skriver ut  hela arrayen
+console.log("Alla maträtter:", dishes);
+
+//Skriver första maträtten
+console.log("Första maträtten:", dishes[0]);
+
+//Skriver sista maträtten
+console.log("Sista maträtten:", dishes[dishes.length - 1]);
+
+//Lägger till ny maträtt i slutet
+dishes.push("Lasange");
+
+//Tar bort första maträtten
+dishes.shift();
+
+//Skriver ut den ändrade arrayen
+console.log("Ändrad array:", dishes);
+
