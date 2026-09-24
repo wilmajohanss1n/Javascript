f14e0d766a74b1636ace418b21e1c0d59296c190
Wilma <wijo2604@student.miun.se>
2026-09-24 17:18:32 +0200

    Lade till villkor för ålderskontroll i uppgift 3

--- uppgift3.js
+++ uppgift3.js
@@ -1,2 +1,14 @@
 /* Lösning till uppgift 3. Av Wilma Johansson, 2026 */
 "use strict";
+
+// Ålder som ska kontrolleras
+const age = 20;
+
+// Kontrollerar vilken åldersgrupp personen tillhör
+if (age < 18) {
+    console.log("Barn");
+} else if (age < 65) {
+    console.log("Vuxen");
+} else {
+    console.log("Pensionär");
+}
